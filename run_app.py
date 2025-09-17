060d1549416e4ab7c9edbad7c3d5eb085e499d65
Aman-Worxwide <amanpreet@worxwide.com>
2025-09-17 12:20:00 +0530

    added procfile

--- run_app.py
+++ run_app.py
@@ -6,19 +6,17 @@ import signal
 
 def main():
     print("Starting")
-    
-    try:
-        backend_process = subprocess.Popen(
-            ["uvicorn", "main:app", "--reload"],
-            cwd="backend"
-        )
-        time.sleep(2)
-        frontend_process = subprocess.Popen(
-            ["streamlit", "run", "app.py"],
-            cwd="frontend"
-        )
-        backend_process.wait()
-        frontend_process.wait()
+    backend_process = subprocess.Popen(
+        ["uvicorn", "main:app", "--reload"],
+        cwd="backend"
+    )
+    time.sleep(2)
+    frontend_process = subprocess.Popen(
+        ["streamlit", "run", "app.py"],
+        cwd="frontend"
+    )
+    backend_process.wait()
+    frontend_process.wait()
         
 
 if __name__ == "__main__":
